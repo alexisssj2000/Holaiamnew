040d8f499a7d6d356dc0a3e68e25e6bd9e51e78b
alexisssj2000 <angelalexisgallegosbanales@gmail.com>
2026-06-01 22:52:10 -0600

    ss

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.development.ts
+export const environment = {
+  production: true,
+  apiUrl: 'https://aboutme-32oq.onrender.com/api',      // Para peticiones HTTP (GET, POST, etc.)
+  staticUrl: 'https://aboutme-32oq.onrender.com'         // Para imágenes estáticas (/uploads)
+};   
